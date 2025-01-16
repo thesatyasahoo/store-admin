60e2415ebddadc9f726dc7633ee8ee2455c8b4df
SATYA-PC\satya <satyasahoo525@gmail.com>
2025-01-16 19:38:44 +0530

    added build files

---
+++ build/@/assets/toast-DYbhU_bb.js
@@ -0,0 +1,1 @@
+import{X as r}from"./index-BJRbsysg.js";const o=e=>r.success(e,{position:"top-center",autoClose:3e3,hideProgressBar:!1,closeOnClick:!0,pauseOnHover:!0,draggable:!0,progress:void 0}),t=e=>r.error(e,{position:"top-center",autoClose:3e3,hideProgressBar:!1,closeOnClick:!0,pauseOnHover:!0,draggable:!0,progress:void 0});export{t as a,o as n};
